063071a3903b643324092388f880464a6dc93e84
bloodbreaker <bloodbreaker0111@gmail.com>
2015-06-22 20:23:10 +0200

    create the Xcode project

---
+++ CityRunning/CityRunning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityRunning
+//
+//  Created by Yan Zhang on 6/22/15.
+//  Copyright (c) 2015 Yan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
